03b7ba562f02d6c68b1f9a12d8d19cf349fd8c8a
t3t3c <kleyguy@gmail.com>
2022-06-12 23:14:19 +0200

    added user prompt that creates a grid with a chosen size

--- main.js
+++ main.js
@@ -12,6 +12,12 @@ function createGrid(size) {
       row.append(pixel);
     }
   }
+  addHoverEffect();
+}
+
+function removeOldGrid() {
+  let gridToRemove = document.querySelectorAll(".pixel, .row");
+  gridToRemove.forEach((element) => element.remove());
 }
 
 function addHoverEffect() {
@@ -23,5 +29,36 @@ function addHoverEffect() {
     });
   });
 }
+
+function anwserFunny(userInput) {
+  if (userInput > 80) {
+    alert("Woah, That's a huge grid. Let's go!");
+  } else if (userInput < 6) {
+    alert("That's pretty small... But I can work with that");
+  }
+}
+
+function correctAnswer(userInput) {
+  if (userInput <= 100 && userInput >= 1) {
+    anwserFunny(userInput);
+    return true;
+  } else {
+    alert("You have to input a number between 1 and 100, Try one more time");
+    return false;
+  }
+}
+
+function userCreateGrid() {
+  button = document.querySelector("button.newGrid");
+  button.addEventListener("click", (e) => {
+    let userInput = Number(
+      prompt("How big do you want the grid to be? Type a number from 1 to 100")
+    );
+    if (correctAnswer(userInput)) {
+      removeOldGrid();
+      createGrid(userInput);
+    }
+  });
+}
 createGrid(16);
-addHoverEffect();
+userCreateGrid();
